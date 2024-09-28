8947558b6412eea13f263735575cb0cb66ca9511
James Miller <james.rex.miller.4@gmail.com>
2024-09-28 10:42:59 -0600

    Set refresh time to 60fps, save game time to dt

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import pygame
 from constants import *
 
 def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -10,9 +15,8 @@ def main():
                 return
         screen.fill("#000000")
         pygame.display.flip()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":

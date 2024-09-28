7c9fde2d10cc8d1e11e8b6f9d5708135f584be0a
James Miller <james.rex.miller.4@gmail.com>
2024-09-28 13:00:34 -0600

    Make fire rate/shooting cooldown 0.3

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
         super().__init__(x, y, radius)
 
         self.rotation = 0
+        self.timer = 0
     
     def triangle(self) -> list:
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -22,6 +23,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.timer -= dt
 
         # rotate left
         if keys[pygame.K_a]:
@@ -44,5 +46,8 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if self.timer > 0:
+            return
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.timer = PLAYER_SHOOT_COOLDOWN
